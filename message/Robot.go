c45ca29369290dc95c060ee0c9bbe591eba0c35a
fadelmp <fadel.puluhulawa@primecash.co.id>
2024-05-01 02:14:51 +0700

    change microservices to monolith architecture

---
+++ message/Robot.go
@@ -0,0 +1,15 @@
+package message
+
+var NameExists = "Name Already Used. Please Create Robot With Another Name"
+var NotFound = "Robot Not Found. Please Check Again"
+var GetSuccess = "Successfully Get Robot"
+var BadRequest = "Bad Request"
+
+var CreateSuccess = "Successfully Create New Robot"
+var CreateFailed = "Failed to Create New Robot"
+
+var UpdateSuccess = "Successfully Update Robot"
+var UpdateFailed = "Failed to Update Robot"
+
+var DeleteSuccess = "Successfully Delete Robot"
+var DeleteFailed = "Failed to Delete Robot"
